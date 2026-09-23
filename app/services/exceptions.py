578036de09721b1d53cad5126c4fb07092fd3db8
BootMerc <ziadshaker56@gmail.com>
2026-09-23 03:32:15 +0300

    service not found exceptions handling

---
+++ app/services/exceptions.py
@@ -0,0 +1,5 @@
+class NotFoundError(Exception):
+# Raised by a service when a requested entity doesn't exist.
+#
+# The global FastAPI exception handler in app/main.py converts this into
+# a 404 response, so individual routers don't need to handle it themselves.
